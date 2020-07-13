c2f8c55ed31d3aed97edfb875512ff7880ccbb63
Matt Mason <masonmj10@gmail.com>
2020-07-13 02:26:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,7 +40,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-return programmer_hash[:alan_kay_is_known_for]
+return programmer_hash [:alan_kay_is_known_for]
+
 end
 
 def dennis_ritchies_language
